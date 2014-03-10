d26cfdfbf1d1f7db8e6def534745677207ac5791
deimos <deimos@deimos-virtual-machine.(none)>
2014-03-10 16:52:35 +0100

    script database for testing

--- source/scenaries/setDatabase.py
+++ source/scenaries/setDatabase.py
@@ -36,15 +36,17 @@ import pdb
 host = sys.argv[1]
 allscenarios = sys.argv[2]
 
+gs = {"Chetumal":0,"Cordoba":1,"Dubai":2,"Irkutsk":3,"Kourou":4,"Krugersdorp":5,"Malaysia":6,"Prince_Albert":7,"Puertollano":8,"Svalbard":9,"Sydney":10,"Troll":11}
+
 def initGroundStations():
-    gs = [("Chetuval",0),("Cordoba",1),("Dubai",2),("Irkutsk",3),("Korou",4),("Krugersdorp",5),("Malysia",6),("Prince_Albert",7),("Puertollano",8),("Svalbard",9),("Sydney",10),("Troll",11)]
+   
   #  pdb.set_trace()
     try:
         con = mdb.connect(host, 'root','','Scenarios')
         cur = con.cursor()
         with con:
             for ground in gs:
-                cur.execute('insert into GroundStations values (%s, %s)',(str(ground[1]),ground[0]))
+                cur.execute('insert into GroundStations values (%s, %s)',(str(gs[ground]),ground))
  
         con.commit()
         print "[GroundStations] Success!"
@@ -118,72 +120,130 @@ def initScenaries(file):
             
         
 def initSatellites(*args):
+    """This functions consists in the initialitation of satellites. This is made by iterating the lines of scenarios files. Once the usefull time of data adquisition of each satellite in each scenario, it will be comparated with each action got from the orbital data by the scenario files.
+The scenario file must be named like "Scenario_NUM_NAMESCENARIO.csv" """
+
     try:
         con = mdb.connect(host,'root','','Scenarios')
         cur = con.cursor()
         data_scenarios = getAllData(args[0][0])
-
-        for doc in args[0][1:]:
+     
+        f = ""
+        try:
+            for doc in args[0][1:]:
     #Format of the file name must be "Scenario_NUM_NAME.csv"
     #where NUM is a interger and NAME can be a undefined string
-            scenarie = ("".join(doc.split("_")[2:])).split(".")[0] 
-            print scenarie
-            lines = []
-            f = open(doc,"r")
-            for line in f:
-                if line != "\n":
-                    # pdb.set_trace()
-                    lines.append(line)
+                scenarie = doc.split("_")[1]
+                lines = []
+                f = open(doc,"r")
+                for line in f:
+                    if line != "\n":
+                        # pdb.set_trace()
+                        lines.append(line)
     
         #pdb.set_trace()
     #Now, we are going to find the num of satellite, the ground station, and the time
     #when the satellite is in the action area of ground station and when it goes out 
     #from the area
-                
-                    for line in lines:
+                #pdb.set_trace()
+                for l in lines:
+                   
         #The missing format is like "GEO-Cloud_005-To-Troll" where 005 is the number
         #of the satellite, Troll is the ground station
-                        if(line.find("GEO") != -1):
-                            sat = line[line.index("_")+1:line.index("_")+4]
-                            gs = line.split("-")[3]
-                            print sat, gs
-                        else:
+                    if(l.find("GEO") != -1):
+                        sat = int(l[l.index("_")+1:l.index("_")+4])
+                        groundStation = l.split("-")[3]
+                       
+                    
+                    else:
             #Whether we are not looking for the Gs or Number of satellite, we are in
-            #data line, in which are been the usefull time, the leave time and the total
+            #data line, in which are been the usefull time for us, the leave time and the total
             #time into the area
-                            line = line.split(",")
-                            start = line[1]
-                            end = line[3]
-                            time = line[5]
-                            print start, end, time
+                        l = l.split(",")
+                        start = l[1]
+                        end = l[3]
+                        time = l[5]
+                        #print groundStation, sat
+                        # print scenarie, data_scenarios[scenarie]
+                        if data_scenarios.has_key(scenarie) and data_scenarios[scenarie].has_key(str(sat)):#if this satellite has usefull time for this scenario
+                            #compare times
+                            for i in data_scenarios[scenarie][str(sat)]:
+                             
+                                if(float(i[0]) >= float(start) and float(i[0]) < float(end)):
+                                    #print i
+                            #times = data_scenarios[scenarie][str(sat)]
+                           # print times
+                         
+                            #print start,end,time, sat, gs[groundStation[:-1]]
+                            #print data_scenarios[scenarie][str(sat)], sat, groundStation
+                            #insert into the database with usefull times
+                                    with con:
+                                        cur.execute("insert into Satellites values(%s,%s,%s,%s,%s,%s,%s)",(sat,scenarie,gs[groundStation[:-1]],start, end,data_scenarios[scenarie][str(sat)][0][0],data_scenarios[scenarie][str(sat)][0][1]))
+                                    con.commit()
 
-    except IOException:
-        print "[InitSatellites] Error with file!"
+                            #     print iteration, sat, scenarie
+                            #     print len(data_scenarios[scenarie][str(sat)])
+                        else:
+                            #inser into the database without usefull times
+                            with con:
+                                cur.execute("insert into Satellites values(%s,%s,%s,%s,%s,%s,%s)",(sat,scenarie,gs[groundStation[:-1]],start, end,-1,-1))
+                            con.commit()
+              
+                    
+
+           # print data_scenarios
+        except IOError:
+            print "[InitSatellites] Error with file!"
+        finally:
+            if f:
+                f.close()
+
+        print "[InitSatellites] Success!"
     except(mdb.IntegrityError,mdb.DatabaseError,mdb.InterfaceError,mdb.Error):
         print "[InitSatellites] DataBaseError ocurred!"
     finally:
-        if f:
-            f.close()
         if con:
             con.close()
     
 def getAllData(file):
-    res ={}
-    f = open(args[0][0],"r")
-    for lines in f:
-        line = lines.split(",")
-        if(line[0]!="Scenario" and len(line[0]) != 0):#into the first state
-            
+    """This function gets the important data for scenarios that are when the satellites
+    came into the interesting area and when they go out of that.
+    For this, we are going to create a dictonary names scenarios that will contains, 
+    ordered by id scenario, all those interactions the satellites will realize
+    The format of scenarios is the next:
+    {scenario:{sat:[(num,num),...], sat:[(num,num),...]}, scenario:{sat:{[(num,num),...], sat:[(num,num),...]}} """
 
+    scenarios ={}
+    try:
+       # pdb.set_trace()
+        f = open(file,"r")
+        for lines in f:
+            line = lines.split(",")
+         #storage in scenario the time in which the satellite came into the zone, which is the satellite and when it comes out the zone
+    
+            if(line[0]!="Scenario"):
+                if(len(line[0]) != 0):#into the first state, contains the first satellite action
+                    idscenario = line[0]
+                    scenarios [idscenario] = {line[3]:[(line[4],line[5])]}
+                    # print (scenarios[idscenario])[line[3]]
+                elif(len(line[3]) != 0):#into the second state that contains a satellite action
+                    if(scenarios[idscenario].has_key(line[3])):
+                        scenarios[idscenario][line[3]].append((line[4],line[5]))
+                    else:
+                        scenarios[idscenario][line[3]] = [(line[4],line[5])]
+    except IOError:
+        print "[GetData] Error with file!"
+
+    return scenarios
 
 def dropDatabase():
     try:
         con = mdb.connect(host, 'root','','Scenarios')
         cur = con.cursor()
         with con:
+            cur.execute('delete from Satellites')
             cur.execute('delete from GroundStations')
             cur.execute('delete from Scenarios')
-            cur.execute('delete from Satellites')
         con.commit()
     except (mdb.DatabaseError,mdb.Error,mdb.InterfaceError):
         if con:
