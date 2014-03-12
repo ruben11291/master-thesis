1e433b4c1ca2b5276c95bc3a38687a0b9b0592df
deimos <deimos@deimos-virtual-machine.(none)>
2014-03-12 17:25:48 +0100

    Orchestrator doc added, satelitevw script started

--- source/scripts/salelitevw.py
+++ source/scripts/salelitevw.py
@@ -23,6 +23,7 @@ import sys
 import os
 from ftplib import FTP
 import MySQLdb as mdb
+import sched,time
 
 """This script simulates the behaviour of a satellite
 Must be executed by "python <id> <scenario> <hostDatabase>"
@@ -33,23 +34,67 @@ scenario: is the scenario that will be simulated
 hostDatabase: is the host where the MySQL database is located
 """
 
-if(len(sys.argv) != 4):
-    print "Error with arguments. Must introduce the satellite's id, scenario and host to ftp"
-    exit(-1)
 
-id = sys.argv[1]
-scenario = sys.argv[2]
-host = sys.argv[3]
+class Satellite:
+    usefull_priority = 4
+    useless_priority = 2
 
-try:
-    con = mdb.connect(host,'root','','Scenarios')
-    cur = con.cursor()
-    with con:
-        res = cur.execute('select idGrounStation,timeInStation,timeOutStation from Satellites where idSatellite = '+id+' and Scenarie = "'+scenario+'"')
-        print res
-    con.close()
-except Exception as e:
-    print "Exception ", e
+    def __init__(self,id,scenario,host):
+        
+        try:
+            con = mdb.connect(host,'root','','Scenarios')
+            cur = con.cursor()
+            satellite_info = 'select * from Satellites where idSatellite=%s and Scenario=%s ORDER BY ;'%(id,scenario)
+            scenario_times = 'select timeIni,timeEnd from Scenarios where Name=%s'%(scenario)
+            with con:
+                cur.execute(satellite_info)
+                self.rows= cur.fetchall()#Getting the Satellite events and its times
+                cur.execute(scenario_times)
+                s_times= cur.fetchall()[0]#Getting when the scenario starts and finishes
+                self.scenario_times = [float(i)/1000 for i in s_times]
+                #Convers the time into seconds 
+                
+            con.close()
+        except Exception as e:
+            print "Exception ", e
+
+    def scheduleBehaviours(self):
+        if self.rows and self.scenario_times:
+            s = sched.scheduler(time.time, time.sleep)
+            print "Scenario times are ",self.scenario_times,type(self.scenario_times[0])
+
+            for seq in self.rows:
+                zone_in_time= float(seq[3])
+                zone_out_time=float(seq[4])
+                ground_station = int(seq[2])
+                if seq[-1] == -1:
+                    s.enter(10,self.useless_priority,self.f1, argument=(str(seq[-2]),))
+                    #I can access to the event
+                    #Maybe could be interesting to create a queue or a reference containing the last running event
+                else:
+                    s.enter(3,self.usefull_priority,self.f1,argument=(str(seq[-1]),))
+                print seq," Scheduled" 
+            s.run()
+            print "after"
+        else:
+            print "[Behaviours] Nothing to schedule"
+            exit(-1)
+
+    def f1(self,time):
+        print "Im the f1 function and print %s" %(time)
+        while(1):
+            None
 
 
-f = FTP('localhost','deimos','deimos')
+if __name__=="__main__":
+    if(len(sys.argv) != 4):
+        print "Error with arguments. Must introduce the satellite's id, scenario and host in which database is located"
+        exit(-1)
+
+    id = sys.argv[1]
+    scenario = sys.argv[2]
+    host = sys.argv[3]
+    sat = Satellite(id,scenario,host)
+    sat.scheduleBehaviours()
+
+#f = FTP('localhost','deimos','deimos')
