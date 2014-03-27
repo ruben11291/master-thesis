9454156ae255a15444b8949a240eb5f80c7affb1
root <root@deimos-virtual-machine.(none)>
2014-03-27 23:29:16 +0100

    starting with PLE

--- source/ple/withaccountPLE.py
+++ source/ple/withaccountPLE.py
@@ -4,18 +4,28 @@ from nepi.execution.ec import ExperimentController
 from nepi.execution.resource import ResourceAction, ResourceState
 import os
 
+def create_node(ec,username,pl_user, pl_password, hostname=None,country=None):
+    
+    node = ec.register_resource("PlanetlabNode")
+    
+    if username:
+        ec.set(node,"username",username)
+    if pl_user:
+        ec.set(node,"pluser",pl_user)
+    if pl_password:
+        ec.
+
 ec = ExperimentController("testple")
 
 # The username in this case is the slice name, the one to use for login in 
 # via ssh into PlanetLab no1des. Replace with your own slice name.
 hostname1 = "planetlab1.u-strasbg.fr"
 hostname2 = "planetlab1.dit.upm.es"
-slice1 = "test1"
-slice2 = "test2"
+slice = "ibbtple_geocloud"
+
 
 name1= "wa8il8im88ge"
 
-#username="wal8il8im88ge_as"
 ssh_key = "/home/deimos/.ssh/id_rsa"
 node1 = ec.register_resource("LinuxNode")
 ec.set(node1,"hostname",hostname1)
