0825e0f4d3d4d99c7b3868152ae868961f36b251
deimos <deimos@deimos-virtual-machine.(none)>
2014-06-09 18:09:46 +0200

    multiprocessing

--- source/gui/experimentController.py
+++ source/gui/experimentController.py
@@ -1,9 +1,31 @@
+
+#!/usr/bin/env python
+
+#
+#    Copyright (C) 2014 DEIMOS
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Ruben Perez <ruben.perez@deimos-space.com>
+
+
 import os
 import sys
 import threading
 from jfedProcXml import JFedParser
-
-threadLock = threading.Lock()
+import pdb
+from ssh_order import SSH_order_thread
 
 class ExperimentController():
     
@@ -13,25 +35,59 @@ class ExperimentController():
         parser = JFedParser("resources/jfed.out")
         self.satellites = parser.get_satellites()
         self.groundstations = parser.get_groundStations()
+        self.satellites_finish= threading.Lock()
+        self.condition_satellite=threading.Lock()
+        self.logs_sem = threading.Lock()
+        self.loads_sem = threading.Lock()
 
+        self.init_timers(self.logs_sem, self.loads_sem)
+
+        #self.semaphore = threading.Semaphore(-len(self.satellites))
+
+
+    def init_timers(self,logs_sem,loads_sem):
+         self.timer_pp_log= QtCore.QTimer(self,logs_sem)
+         self.timer_pp_log.setInterval(3000)
+         self.timer_pp_log.timeout.connect(self.update_log)
+         
+         self.timer_orch_log =QtCore.QTimer(self,logs_sem)
+         self.timer_orch_log.setInterval(3000)
+         self.timer_orch_log.timeout.connect(self.update_log)
+         
+         self.timer_pp_load= QtCore.QTimer(self,loads_sem)
+         self.timer_pp_load.setInterval(3000)
+         self.timer_pp_load.timeout.connect(self.update_load)
+         
+         self.timer_orch_load =QtCore.QTimer(self,loads_sem)
+         self.timer_orch_load.setInterval(3000)
+         self.timer_orch_load.timeout.connect(self.update_load)
+
+         
+    def logs_sem(self):
+        
     def start_satellites(self):
-        self.sat_threads=List()
+        self.sat_threads=[]
         num = 1
         for sat in self.satellites:
-            t=SSH_order_thread(sat,self,"Satellite",num)
+            order = "'python satellite.py %s %s `cat ipdb`'"%(num, self.getScenario())
+            t=SSH_order_thread(sat,self,"Satellite",num,order,self.threadLog)
             self.sat_threads.append(t)
+            num+=1
         for sat in self.sat_threads:
             sat.start()
             self.log("Satellite %d started!"%(sat.getId()))
-        
+       
         self.log("Satellites started!")
             
     def start_ground(self):
-        self.gs_threads=List()
+        self.gs_threads=[]
         num = 0
+        pdb.set_trace()
         for gs in self.groundstations:
-            t=SSH_order_thread(gs,self,"Ground Station",num)
+            order = "'python groundstation.py %s %s `cat ipdb` &'"%(num,self.getScenario())
+            t=SSH_order_thread(gs,self,"Ground Station",num,order,self.threadLog)
             self.gs_threads.append(t)
+            num+=1
         for gs in self.gs_threads:
             gs.start()
             self.log("Ground Station %d started!"%(gs.getId()))
@@ -41,43 +97,53 @@ class ExperimentController():
         for gs in self.gs_threads:
             if gs.isAlive():
                 gs.stop()
+
             self.gs_threads.remove(gs)
             self.log("Ground Station %d stopped!"%(gs.getId()))
+            
+    
    
     def clean_ground(self):
         for sat in self.satellites:
-            os.system("ssh -A -i /home/deimos/.ssh/id_rsa jbecedas@%s"%(sat) + " -oPort=22 -oProxyCommand='ssh -e none -i /home/deimos/Descargas/emulabcert.pem -oPort=22 jbecedas@bastion.test.iminds.be nc -w 5 %h %p' 'ls -al'")
+            os.system("ssh -A -i /home/deimos/.ssh/id_rsa jbecedas@%s"%(sat) + " -oPort=22 -oProxyCommand='ssh -e none -i /home/deimos/Descargas/emulabcert.pem -oPort=22 jbecedas@bastion.test.iminds.be nc -w 5 %h %p' 'killall python'")
         self.log("Cleaned Satellites")
         for gs in self.groundstations:
-            os.system("ssh -A -i /home/deimos/.ssh/id_rsa jbecedas@%s"%(gs) + " -oPort=22 -oProxyCommand='ssh -e none -i /home/deimos/Descargas/emulabcert.pem -oPort=22 jbecedas@bastion.test.iminds.be nc -w 5 %h %p' 'ls -al'")
+            os.system("ssh -A -i /home/deimos/.ssh/id_rsa jbecedas@%s"%(gs) + " -oPort=22 -oProxyCommand='ssh -e none -i /home/deimos/Descargas/emulabcert.pem -oPort=22 jbecedas@bastion.test.iminds.be nc -w 5 %h %p' 'killall python'")
         self.log("Cleaned Ground Stations")
             
+    def setScenario(self,scenario):
+        self.scenario = scenario
+
+    def getScenario(self):
+        if self.scenario:
+            return self.scenario
+        else:
+            return None
+
     def log(self,msg):
         if self.widget:
-            self.threadLog.acquire()
             self.widget.log(msg)
-            self.threadLog.release()
 
-class SSH_order_thread(threading.Thread):
-    def __init__(self,host,experimentController,msg,id):
-        self.host = host
-        self.experimentController = experimentController;
-        self.msg = msg
-        self.id = id
-    
-    def getHost(self):
-        return self.host
-    def getId(self):
-        return self.id
+    def error(self,thread):
+        if thread.isAlive():
+            thread._Thread__stop()
+        if self.widget:
+            raise DeployingException()
 
-    def run(self):
-        os.command("ssh -A -i /home/deimos/.ssh/id_rsa jbecedas@n144-09a.wall1.ilabt.iminds.be -oPort=22 -oProxyCommand='ssh -e none -i /home/deimos/Descargas/emulabcert.pem -oPort=22 jbecedas@bastion.test.iminds.be nc -w 5 %h %p' 'ls -al'")
-        threadLock.acquire()
-        self.experimentController.log(self.msg +" "+self.id+ " ha acabado su ejecucion")
-        threadLock.release()
+    def run(self,scenario):
+        self.setScenario(scenario)
+        self.start_ground(scenario)
+        time.sleep(5.0)
+        self.start_satellites(scenario)
+        if self.widget:
+            self.widget.scenarioInitiated()
+  
+       
 
 if __name__ == "__main__":
     e = ExperimentController()
-    a = "ssh -A -i /home/deimos/.ssh/id_rsa jbecedas@%s" %("hostmachine")+ " -oPort=22 -oProxyCommand='ssh -e none -i /home/deimos/Descargas/emulabcert.pem -oPort=22 jbecedas@bastion.test.iminds.be nc -w 5 %h %p' 'ls -al'"
-    print a
-    e.clean_ground()
+    e.setScenario(1)
+    e.start_ground()
+    print "STOPP"
+    e.start_satellite()
+    e.clean()
---
+++ source/gui/ssh_order.py
@@ -0,0 +1,57 @@
+
+#!/usr/bin/env python
+
+#
+#    Copyright (C) 2014 DEIMOS
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Ruben Perez <ruben.perez@deimos-space.com>
+
+
+class SSH_order_thread(QtCore.QThread):
+
+    __pyqtSignals__=("order_complete","error")
+
+    def __init__(self,host,experimentController,msg,id,order,threadLock):
+        threading.Thread.__init__(self)
+        self.host = host
+        self.experimentController = experimentController;
+        self.msg = msg
+        self.id = id
+        self.order=order
+        self.threadLock=threadLock
+
+    def stop(self):
+        thread._Thread__stop()
+
+    def getHost(self):
+        return self.host
+    def getId(self):
+        return self.id
+
+    def getType(self):
+        return self.type
+
+
+    def run(self):
+        print self.host
+        out =os.system("ssh -A -o StrictHostKeyChecking=no -i /home/deimos/.ssh/id_rsa jbecedas@%s"%(self.host)+" -oPort=22 -oProxyCommand='ssh -o StrictHostKeyChecking=no -e none -i /home/deimos/Descargas/emulabcert.pem -oPort=22 jbecedas@bastion.test.iminds.be nc -w 5 %h %p' "+self.order)
+        self.threadLock.acquire()
+        self.experimentController.log(str(self.msg) +" "+str(self.id)+ " ha acabado su ejecucion")
+        self.threadLock.release()
+        print "Out ",out
+        if out != 0:
+            self.experimentController.error(self)
+       
