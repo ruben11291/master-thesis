83053f05ade01b2a8a0f45fe70ae37a06b9eb7c9
deimos <deimos@deimos-virtual-machine.(none)>
2014-06-11 18:44:04 +0200

    integration catalog

--- source/bonfire/geoserver/catalog_pp.py
+++ source/bonfire/geoserver/catalog_pp.py
@@ -1,4 +1,8 @@
+ #!/usr/bin/env python
+
+
 #!/usr/bin/env python
+
 #
 #    Copyright (C) 2014 DEIMOS
 #
@@ -16,7 +20,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Author: Ruben Perez <ruben.perez@deimos-space.com>
-
 import pdb
 from geoserver.catalog import Catalog
 from geoserver.workspace import Workspace
@@ -26,36 +29,58 @@ from geoserver.catalog import Catalog, ConflictingDataError, UploadError, \
     FailedRequestError
 #from AyC import catalog
 import sys
-
+import os
 geoserver_path = "http://localhost:80/geoserver/rest"
 
+def sendToCatalog(catalog, path):
+	 name = path.split("/")[-1]
+         wksp = None
+         pdb.set_trace()
+         while True:
+             try:
+                 wksp = catalog.createWkspace(name)
+                 break
+             except Exception:
+		 None                 
+	 print path
+         catalog.addImage(name,wksp,path)
+#pdb.set_trace()
+#catalog = catalog.catalog(geoserver_path)
+#sendToCatalog(catalog,sys.argv[1])
 
 if __name__ == "__main__":
-        if (len(sys.argv) != 2):
-                print "Error with arguments. Must enter almost two"
-                sys.exit(-1)
-        print sys.argv[1]
-        name_file=sys.argv[1].split("/")[-1]
-        scenario="Scenario1"
-        cat = Catalog(geoserver_path)
-        #pdb.set_trace()
+	if (len(sys.argv) != 3):
+		print "Error with arguments. Must enter almost two"
+		sys.exit(-1)
+	print sys.argv[1]
+	name_file=sys.argv[1].split("/")[-1]
+	scenario=sys.argv[2]
 
-        try:
-                print name_file,sys.argv[1]
-                vk = cat.get_workspace(scenario)
-                if vk ==None:
-                        vk=cat.create_workspace(scenario,scenario)
-                vk.enabled=True
-                cv = cat.create_coveragestore("test",sys.argv[1],vk)
-                inf=CoverageStore(cat,vk,scenario)
-                #inf.fetch()
-                cat.save(inf)
-                f.write("Processed")
-        except ConflictingDataError as e:
-                print "Exception ",e
-        except UploadError as e:
-                print "Exception ",e
-        except FailedRequestError  as e:
-                print "Exception ",e
+	cat = Catalog(geoserver_path)
+	#pdb.set_trace()
+		
+	try:
+		print name_file,sys.argv[1]
+		vk = cat.get_workspace(scenario)
+		if vk ==None:
+			vk=cat.create_workspace(scenario,scenario)
+		vk.enabled=True
+		try:
+			cv = cat.create_coveragestore("test",sys.argv[1],vk)
+			inf=CoverageStore(cat,vk,scenario)
+			inf.fetch()
+			cat.save(inf)
+			f.write("Processed")
+		except Exception:
+			print "Catalogued!"
+		comman ='curl -i --data "name=%s&imageurl=ftp://131.254.204.143:21/../../usr/share/tomcat7/apache-tomcat-7.0.53/webapps/geoserver/data/data/%s/%s/%s.geotiff" http://172.18.250.13:8043/IDV'%(name_file,scenario,"test","test")
+		print comman
+		os.system(comman)
 
-
+	except ConflictingDataError as e:
+		print "Exception ",e
+	except UploadError as e:
+		print "Exception ",e
+	except FailedRequestError  as e:
+		print "Exception ",e
+	
